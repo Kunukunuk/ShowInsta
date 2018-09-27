8fc064ef15e031271341dfb147b02a05c69cca9c
Kun Huang <kkun.huang@gmail.com>
2018-09-26 23:15:01 -0400

    home feed view controller

---
+++ ShowInsta/ViewController/HomeFeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeFeedViewController.swift
+//  ShowInsta
+//
+//  Created by Kun Huang on 9/26/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
