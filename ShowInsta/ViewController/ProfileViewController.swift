43c075a302eda95c9df0896387da51933a27ef97
Kun Huang <kkun.huang@gmail.com>
2018-10-04 17:39:13 -0400

    change profile picture no data persist

--- ShowInsta/ViewController/ProfileViewController.swift
+++ ShowInsta/ViewController/ProfileViewController.swift
@@ -9,22 +9,68 @@
 import UIKit
 import Parse
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    var imagePicker = UIImagePickerController()
+    var takenProfile: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imagePicker.delegate = self
         profileImageView.image = UIImage(named: "Profile")
         getName()
         // Do any additional setup after loading the view.
     }
     
+    func setProfilePicture() {
+        profileImageView.image = takenProfile
+    }
+    
+    func alertSheet() {
+        let alertSheet = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        
+        alertSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in self.useCamera()}))
+        alertSheet.addAction(UIAlertAction(title: "Photo", style: .default , handler: { _ in self.usePhoto()}))
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        cancelAction.setValue(UIColor.red, forKey: "titleTextColor")
+        alertSheet.addAction(cancelAction)
+        
+        present(alertSheet, animated: true, completion: nil)
+    }
+    
+    func useCamera() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("camera not availble")
+            imagePicker.sourceType = .photoLibrary
+        }
+    }
+    
+    func usePhoto() {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     @IBAction func changeProfilePic(_ sender: UITapGestureRecognizer) {
-        print("hello")
+        alertSheet()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            takenProfile = image
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        setProfilePicture()
     }
     
     func getName() {
